c87210efe3a9f85668267727ae591afedd4ddc0c
Arana <it21038150@my.sliit.lk>
2023-04-29 11:30:07 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express()
 
 // CORS
 const corsOptions = {
-    origin: ["https://localhost:3000", "https://dapass-v1.netlify.app"]
+    origin: ["https://localhost:3000", "onepass-vault-v2.netlify.app"]
 }
 app.use(cors(corsOptions))
 
