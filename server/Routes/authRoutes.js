d62126870f3f5bf4482092979f52e13451ed2e79
Arana <it21038150@my.sliit.lk>
2023-07-30 23:55:27 +0530

    misc

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -8,7 +8,7 @@ router.post("/auth/genKeys", userKeyGeneration)
 router.post("/auth/add-user-data", addData)
 router.post("/auth/signin-request", signInRequest)
 router.post("/auth/signin", signIn)
-router.post("/auth/token",requireSignin, tokenRefresh)
+router.post("/auth/token", tokenRefresh)
 router.post("/auth/signout", signOut)
 
 export default router
