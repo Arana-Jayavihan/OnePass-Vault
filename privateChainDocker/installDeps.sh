be66981de2a24c9e7e7826806bdade79c902349e
root <root@testServer.qlp5gknhyy1exn13oiv41hzbka.ix.internal.cloudapp.net>
2023-09-11 20:07:24 +0000

    Automated Blockchain deployment on docker

---
+++ privateChainDocker/installDeps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# Installing minor dependancies
+sudo apt-get install python
+sudo apt-get install gpg
+sudo apt-get install firewalld
+
+# Installing Docker
+# Add Docker's official GPG key:
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# Installing GETH
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
