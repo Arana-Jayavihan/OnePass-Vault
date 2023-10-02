0ad59d40e4ed2d7de025fdb96da75740bde65fa2
Arana Jayavihan <aranajayavihan@gmail.com>
2023-10-02 12:31:41 +0530

    Impproved Authentication

--- client-onepass/src/helpers/urlConfigs.js
+++ client-onepass/src/helpers/urlConfigs.js
@@ -1,5 +1,3 @@
-//export const api = "http://testserver-onepass-rpc.southeastasia.cloudapp.azure.com:9000/api"
+//export const api = "http://127.0.0.1:9000/api"
 
-export const api = "http://127.0.0.1:9000/api"
-
-//export const api = "https://onepass-vault-production.up.railway.app/api"
+export const api = "https://onepass-vault-production.up.railway.app/api"
